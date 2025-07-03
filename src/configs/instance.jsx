92489046ef090d2d63672cded00549341a333613
Nguyễn Minh Hiếu <124897137+Hieu-250903@users.noreply.github.com>
2025-07-03 10:10:31 +0700

    update instance

--- src/configs/instance.jsx
+++ src/configs/instance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookie from "js-cookie";
 
 const instance = axios.create({
-  baseURL: "https://localhost:7131/api",
+  baseURL: "https://api.nhannguyen.site/api",
 });
 
 instance.interceptors.request.use(
